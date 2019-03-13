5e8a776179be05c9eea0f2faeb7cbeb5c86d5c0c
marzuk12 <marzuk12@gmail.com>
2019-03-13 07:05:39 -0400

    Add jQuery library and use in script.js

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,14 +1,7 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    var checkboxes = document.getElementsByTagName("input");
-    for (var i = 0; i < checkboxes.length; i++) {
-        checkboxes[i].addEventListener("click", clickHandler);
-    }
+$(function() {
+    $("input").on("click", function() {
+        $(this)
+            .parent()
+            .toggleClass("checked");
+    });
 });
-
-function clickHandler() {
-    if (this.checked) {
-        this.parentNode.className = "checked";
-    } else {
-        this.parentNode.className = "";
-    }
-}
